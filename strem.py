ebe5c248474c3d8fb27a3078324834a903a0f5c7
Kbenks <37184679+Kbenks@users.noreply.github.com>
2022-07-28 02:32:57 +0100

    Update strem.py

--- strem.py
+++ strem.py
@@ -228,7 +228,7 @@ def face_detector(img_path):
    # return np.argmax(ResNet50_model.predict(img))
 
 ### returns "True" if a dog is detected in the image stored at img_path
-def dog_detector(img_path):
+#def dog_detector(img_path):
    # prediction = ResNet50_predict_labels(img_path)
    # return ((prediction <= 268) & (prediction >= 151))
 from PIL import Image
