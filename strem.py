4d805824e8450c2492b3ab0d015a7fcc9400f72d
Kbenks <37184679+Kbenks@users.noreply.github.com>
2022-07-28 01:28:04 +0100

    Update strem.py

--- strem.py
+++ strem.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from tensorflow import keras
-VGG19_model_aug = keras.models.load_model('./saved_models')
+from tensorflow.keras.applications.resnet50 import ResNet50
+Xception_model_aug = keras.models.load_model('./mod')
+ResNet50_model=keras.models.load_model('./dogdetect')
 from sklearn.datasets import load_files       
 from keras.utils import np_utils
 import numpy as np
@@ -13,8 +15,13 @@ from keras.models import Sequential
 from keras.utils import img_to_array
 from keras.utils import load_img
 from keras.callbacks import ModelCheckpoint             
-from tqdm import tqdm              
+from tqdm import tqdm
+import cv2                
 import matplotlib.pyplot as plt
+import random
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.utils import img_to_array
+from keras.utils import load_img
 
 # load list of dog names
 dog_names = ['1.Affenpinscher',
@@ -149,25 +156,87 @@ dog_names = ['1.Affenpinscher',
  '0.Welsh_springer_spaniel',
  '1.Wirehaired_pointing_griffon',
  '2.Xoloitzcuintli',
- '3.Yorkshire_terrier']
-
-def extract_VGG19(tensor):
-	from keras.applications.vgg19 import VGG19, preprocess_input
-	return VGG19(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
-
-from PIL import Image
+ '3.Yorkshire_terrier',
+ '4.Rottweiller',
+ '5.Staffordshire_bull_terrier',
+ '6.Pomsky',
+ '7.Dutch_shepherd',
+ '8.Aidi',
+ '9.Beldi_moroccan',
+ '0.Argentino_dogo',
+ '1.Czechoslovakian_Wolfdog',
+ '2.Rhodesian_ridgeback',
+ '3.Jack_russell_terrier',
+ '4.Fila_brasileiro',
+ '5.Presa_canario',
+ '6.Spanish_mastiff',
+ '7.Russiky_toy',
+ '8.Boerboel',
+ '9.American_pitbull_terrier',
+ '0.American_bulldog',
+ '1.American_bully',
+ '2.Continental_bulldog']
 
 def load_image(image_file):
 	img = Image.open(image_file)
 	return img
+# def extract_VGG19(tensor):
+#	from keras.applications.vgg19 import VGG19, preprocess_input
+#	return VGG19(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+random.seed(8675309)
 
-st.set_page_config( page_title="DOG BREED")
-st.title("WHAT IS THE BREED OF THIS DOG ?")
- 
-def load_image(image_file):
-    img = Image.open(image_file)
-    return img
+# load filenames in shuffled human dataset
+human_files = np.array(glob("./lfw/*/*"))
+random.shuffle(human_files)
+                              
 
+# extract pre-trained face detector
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt.xml')
+
+# load color (BGR) image
+img = cv2.imread(human_files[3])
+# convert BGR image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# find faces in image
+faces = face_cascade.detectMultiScale(gray)
+
+# convert BGR image to RGB for plotting
+cv_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+def extract_Xception(tensor):
+	from keras.applications.xception import Xception, preprocess_input
+	return Xception(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+
+### a function that takes a path to an image as input
+### and returns the dog breed that is predicted by the model.
+def Xception_predict_breed (img_path):
+    # extract the bottle neck features
+    bottleneck_feature = extract_Xception(path_to_tensor(img_path)) 
+    ## get a vector of predicted values
+    predicted_vector = Xception_model_aug.predict(bottleneck_feature) 
+    
+    ## return the breed
+    return dog_names[np.argmax(predicted_vector)]
+
+# returns "True" if face is detected in image stored at img_path
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
+
+def ResNet50_predict_labels(img_path):
+    # returns prediction vector for image located at img_path
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+### returns "True" if a dog is detected in the image stored at img_path
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return ((prediction <= 268) & (prediction >= 151))
+
+from PIL import Image
 def path_to_tensor(image_file):
     # loads RGB image as PIL.Image.Image type
     img = load_img(image_file, target_size=(224, 224))
@@ -179,18 +248,36 @@ def path_to_tensor(image_file):
 def paths_to_tensor(image_paths):
     list_of_tensors = [path_to_tensor(image_file) for image_file in tqdm(image_paths)]
     return np.vstack(list_of_tensors)
-def VGG19_predict_breed (image_file):
+
+def Xception_predict_breed (image_file):
     # extract the bottle neck features
-    bottleneck_feature = extract_VGG19(path_to_tensor(image_file)) 
+    bottleneck_feature = extract_Xception(path_to_tensor(image_file)) 
     ## get a vector of predicted values
-    predicted_vector = VGG19_model_aug.predict(bottleneck_feature) 
+    predicted_vector = Xception_model_aug.predict(bottleneck_feature) 
     
     ## return the breed
     return dog_names[np.argmax(predicted_vector)]
-def breed_identifier(image_file):
-    load_image(image_file)
-    prediction = VGG19_predict_breed(image_file)
-    return st.write(f"This dog is a {prediction}\n")
+
+    
+def breed_identifier(img_path):
+    prediction = Xception_predict_breed(img_path)
+    if dog_detector(img_path) == True:
+        st.write('picture is a dog')
+        return st.write(f"This dog is a {prediction}\n")
+    
+    if face_detector(img_path) == True:
+        st.write('This is a human, "BACHARE" as we Moroccan say')
+        return st.write(f"This person looks like a {prediction}\n")
+        
+    
+    else:
+        return st.write(f'Not sure if it is a dog, if so it is a {prediction}\n')
+
+st.set_page_config( page_title="DOG BREED")
+st.title("WHAT IS THE BREED OF THIS DOG ?")
+ 
+
+
 
 
 image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
