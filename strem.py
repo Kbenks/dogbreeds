c3e43a3d2837a9eaad221eda95eefb7c631c2eea
Kbenks <37184679+Kbenks@users.noreply.github.com>
2022-07-28 02:31:33 +0100

    Update strem.py

--- strem.py
+++ strem.py
@@ -231,8 +231,8 @@ def face_detector(img_path):
 def dog_detector(img_path):
    # prediction = ResNet50_predict_labels(img_path)
    # return ((prediction <= 268) & (prediction >= 151))
-
 from PIL import Image
+
 def path_to_tensor(image_file):
     # loads RGB image as PIL.Image.Image type
     img = load_img(image_file, target_size=(224, 224))
