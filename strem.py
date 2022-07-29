29817fae7588d110d9ba8f00a0919de5a7e7ea75
Kbenks <37184679+Kbenks@users.noreply.github.com>
2022-07-29 14:42:38 +0100

    Update strem.py

--- strem.py
+++ strem.py
@@ -179,8 +179,7 @@ def save_uploaded_file_right(uploaded_file):
     #try:
 
      with open(os.path.join('images_right',uploaded_file.name),'wb') as f:
-
- 	f.write(uploaded_file)
+		f.write(uploaded_file)
 
         #return 1    
 
@@ -193,8 +192,7 @@ def save_uploaded_file_wrong(uploaded_file):
     #try:
 
      with open(os.path.join('images_wrong',uploaded_file.name),'wb') as f:
-
- 	f.write(uploaded_file)
+		f.write(uploaded_file)
 
         #return 1    
 
